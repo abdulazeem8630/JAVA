131f8554d1099a57c3faaf11615cd4310ef8cc37
Abdul Azeem <abdulazeem863062@gmail.com>
2026-08-09 17:57:20 +0530

    add Packages

---
+++ _5_Packages/src/azeem/_Another_class.java
@@ -0,0 +1,13 @@
+package azeem;
+import com.azeem.modal.Main;
+import static java.lang.Math.*;
+//import static java.lang.Math.sqrt;
+
+public class _Another_class {
+    static void main(String[] args) {
+        Main mn = new Main();
+        mn.sayHello();
+        System.out.println(Math.sqrt(25));
+        System.out.println(pow(2, 3));
+    }
+}
---
+++ _5_Packages/src/com/azeem/modal/Main.java
@@ -0,0 +1,12 @@
+package com.azeem.modal;
+
+public class Main {
+    public void sayHello(){
+      System.out.println("Hey....");
+    }
+  static void main(String[] args) {
+    System.out.println("Hello...");
+    Main m = new Main();
+    m.sayHello();
+  }
+}
