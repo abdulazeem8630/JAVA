bba168a88f2d3700748493bc17a81fae41686b26
Abdul Azeem <abdulazeem863062@gmail.com>
2026-08-09 17:23:54 +0530

    add basics of exception handling

--- _4_Java_OOPs/src/_9_Exception_Handling.java
+++ _4_Java_OOPs/src/_9_Exception_Handling.java
@@ -1,2 +1,15 @@
 public class _9_Exception_Handling {
+    static void main(String[] args) {
+        try{
+            int x = 10/0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        catch (Exception e) {
+            System.out.println("Exception Handled");
+        }//finally
+        finally {
+            System.out.println("Final msg...");
+        }
+    }
 }
